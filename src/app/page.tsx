6ea409fb3f5edc412f9688b4523f97b63754ab2e
Mehedi Mehad <mdmehedihasanmehad@gmail.com>
2026-02-08 23:00:38 +0600

    feat(home) get featured projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import HeroSectionClient from "@/components/ui/modules/Home/HeroSection/HeroSect
 import ProjectsSection from "@/components/ui/modules/Home/Projects/ProjectsSection";
 import RecentBlogs from "@/components/ui/modules/Home/RecentBlogs/RecentBlogs";
 import ScheduleMeeting from "@/components/ui/modules/Home/ScheduleMeeting/ScheduleMeeting";
-import AuthButtons from "@/components/ui/shared/AuthButtons/AuthButtons";
-import { getEnvVar } from "@/lib/env";
+import { getFeaturedProjectsAction } from "@/services";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const featuredProjects = await getFeaturedProjectsAction();
   return (
     <main>
       <HeroSectionClient />
-      <ProjectsSection />
+      <ProjectsSection featuredProjects={featuredProjects} />
       <RecentBlogs />
       <ScheduleMeeting />
     </main>
